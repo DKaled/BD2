bbea4521f4c331dd93abba34bf03ff264f519eef
Derek <derekyoam98@gmail.com>
2021-05-31 16:52:57 -0500

    31/05/21 ~directivo, V3.0 index,  V2.0 login

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+    session_start();
+
+    if (isset($_SESSION['usuario'])) {
+        if ($_SESSION['usuario']['tipo'] == "admin") {
+            header('Location: http://localhost/bd2/www/admin.php');
+        } else if ($_SESSION['usuario']['tipo'] == "user") {
+            header('Location: http://localhost/bd2/www/usuario.php');
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,24 +22,22 @@
     <link rel="stylesheet" href="bootstrap-5.0.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <script src="js/scripts.js"></script>
-    <script src="bootstrap-5.0.1/js/bootstrap.bundle.min.js"></script>
-    <script src="bootstrap-5.0.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div id="background"></div>
     <div class="login-container">
         <div class="login">
             <div class="login-logo"></div>
-            <form id="loginForm" class="login-form" action="" method="post">
+            <form id="loginForm" class="login-form" action="" method="POST">
                 <label class="login-title">Iniciar Sesión</label>
                 <label for="user" class="text">Usuario</label>
                 <div class="user-input">
-                    <input type="text" name="user" id="user" placeholder="Usuario">
+                    <input type="text" name="user" id="user" requiered pattern="[A-Za-z0-9_-]{1,15}" placeholder="Usuario">
                     <span class="user-icon input-group-text"><i id="userIcon" class="fas fa-user"></i></span>
                 </div>
                 <label for="pass" class="text">Contraseña</label>
                 <div class="pass-input">
-                    <input type="password" name="pass" id="pass" placeholder="Contraseña" data-toggle="password">
+                    <input type="password" name="pass" id="pass" requiered pattern="[A-Za-z0-9_-]{1,15}" placeholder="Contraseña" data-toggle="password">
                     <div class="input-group-append">
                         <span class="eye-pass-icon input-group-text"><i id="eyePassIcon" class="fas fa-eye" onclick="togglePass()"></i></span>
                     </div>
--- php/login.php
+++ php/login.php
@@ -1,29 +1,28 @@
 <?php
-    echo "<script>console.log('hola');</script>";
-    session_start();
-    include_once "conection.php";
-    $con = new Conexion();
-    $con = $con->connect();
-    print_r($con);
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            include_once ('conection.php');
+            $con = new Conexion();
+            $con = $con->connect();
+            //print_r($con);
 
-    //Recepción de datos POST
-    $dbUser = (isset($_POST["userName"])) ? $_POST["userName"] : "";
-    $dbPass = (isset($_POST["pass"])) ? $_POST["pass"] : "";
+            session_start();
+    
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+            $cryptPass = md5($pass);
+            //Y si en vez de usar otra tabla en usuario uso cargo?
+            $query = $con->prepare("SELECT usuario.username, tipousuario.tipo FROM usuario INNER JOIN tipousuario ON usuario.idTipoUsuario = tipousuario.idTipoUsuario WHERE username = ? AND contrasenna = ?");
+            $result = $query->execute(array($user, $cryptPass));
+            $rows = $query->rowCount();
 
-    $cryptDBPass = md5($dbPass);
+            if ($rows && $result) {
+                $data = $query->fetch(PDO::FETCH_ASSOC);
+                $_SESSION['usuario'] = $data;
+                echo json_encode(array('error' => false, 'usuario' => $data['username'], 'tipo' => $data['tipo']));
+            } else {
+                echo json_encode(array('error' => true));
+            }
 
-    $query = "SELECT * FROM usuario WHERE usuario = '$dbUser' AND contraseña = '$cryptDBPass'";
-    $result = $con->prepare($query);
-    $result->execute();
-
-    if ($result->rowCount() >= 1) {
-        $data = $result->fetchAll(PDO::FETCH_ASSOC);
-        $_SESSION["s_user"] = $dbUser;
-    } else {
-        $_SESSION["s_user"] = NULL;
-        $data = NULL;
-    }
-
-    print json_encode($data);
-    $con = NULL;
+            $con = NULL;
+        }
 ?>
